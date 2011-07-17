090042600c59b6755a3242f05b1778eac6f0cac1
Michal Horejsek <horejsekmichal@gmail.com>
2011-07-17 12:09:49 +0200

    move bashrc files into directory

---
+++ bashrc/bashrc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# msh (Michal's Shell)
+# Michal Horejsek <horejsekmichal@gmail.com>
+# https://github.com/horejsek/msh
+#
+
+dir=`dirname $BASH_SOURCE`
+
+. $dir/commands.sh
+. $dir/less.sh
+. $dir/ssh.sh
+
