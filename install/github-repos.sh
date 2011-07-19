7215bbf4990083b0c5f11999bdec6b74f01ee8c3
Michal Horejsek <horejsekmichal@gmail.com>
2011-07-19 23:02:47 +0200

    install github repos

--- install/github-repos.sh
+++ install/github-repos.sh
@@ -12,26 +12,50 @@ if [ ! "$UID" -eq "$ROOT_UID" ]; then
 	exit 1
 fi
 
+## PREPARE
+
 dir=`dirname $BASH_SOURCE`
 cd $dir
 
-gitrepos=`cat github-repos`
+gitrepos_python=`cat github-repos-python`
+gitrepos_python3=`cat github-repos-python3`
 
 if [ -e tmp ]; then
 	rm -rf tmp
 fi
 mkdir tmp && cd tmp
 
-for gitrepo in $gitrepos; do
+## INSTALL REPOS FOR PYTHON
+
+IFS=$'\n'
+for gitrepo in $gitrepos_python; do
+	IFS=$'\n '
 	git clone $gitrepo
 done
 
 for repo in `ls -1 .`; do
 	cd $repo
 	python setup.py install
+	cd ..
+done
+
+rm -rf *
+
+## INSTALL REPOS FOR PYTHON 3
+
+IFS=$'\n'
+for gitrepo in $gitrepos_python3; do
+	IFS=$'\n '
+	git clone $gitrepo
+done
+
+for repo in `ls -1 .`; do
+	cd $repo
 	python3 setup.py install
 	cd ..
 done
+
+## CLEAN
 
 cd ..
 
