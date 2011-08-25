f42d2acea861f03bc390c2b52cee73c25661fcd6
Michal Horejsek <horejsekmichal@gmail.com>
2011-08-25 23:02:47 +0200

    comment

--- msh/git.sh
+++ msh/git.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+#
+# msh (Michal's Shell)
+# Michal Horejsek <horejsekmichal@gmail.com>
+# https://github.com/horejsek/msh
+#
 
 git config --global alias.prettylog 'log --pretty=format:"%h %s" --graph'
 
