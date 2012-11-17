43f6b24c155a27c5d3ec779e5bd3134e4e458203
Michal Horejsek <horejsekmichal@gmail.com>
2012-11-17 11:05:25 +0100

    Aliases for ls, cd, grep, etc.

--- msh/alias.sh
+++ msh/alias.sh
@@ -5,10 +5,29 @@
 # https://github.com/horejsek/msh
 #
 
+alias ls='ls --color=auto'
+alias ll='ls -la'
+
+alias cd..='cd ..'
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../../'
+
+alias grep='grep --color=auto'
+alias ack-grep='ack-grep --color'
+
+alias diff='colordiff'
+
+alias ports='netstat -tulanp'
+
 alias bp='bpython'
+
 alias gitup='git pull origin'
 alias gg='gitg'
 alias gk='gitk'
 
-alias ack-grep='ack-grep --color'
+
 
