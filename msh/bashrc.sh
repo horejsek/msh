d6a2aeef0a628e4bd7d3d7a98d424e5c03ef01ee
Michal Horejsek <horejsekmichal@gmail.com>
2012-11-23 16:55:21 +0100

    Bash prompt

--- msh/bashrc.sh
+++ msh/bashrc.sh
@@ -7,12 +7,16 @@
 
 ROOT_UID=0
 
+PROMPT_USER_COLOR='0;34m'
+PROMPT_HISTORY_COLOR='0;33m'
+PROMPT_DIR_COLOR='0;32m'
 PROMPT_COLOR='1;34m'
 if [ ${UID} -eq ${ROOT_UID} ]; then
-	PROMPT_COLOR='1;31m'
+	PROMPT_USER_COLOR='1;31m'
+    PROMPT_COLOR='1;31m'
 fi
 
-export PS1="[\u@\h \[\e[${PROMPT_COLOR}\]\W\[\e[0m\]]\$ "
+export PS1="[\[\e[${PROMPT_USER_COLOR}\]\u@\h\[\e[0m\]] \t \[\e[${PROMPT_HISTORY_COLOR}\]\!\[\e[0m\] \[\e[${PROMPT_DIR_COLOR}\]\w\[\e[0m\]\n\[\e[${PROMPT_COLOR}\]\$\[\e[0m\] "
 
 export HISTTIMEFORMAT="%d/%m/%y %T "
 
