ebba651edb879684d521c3a48d836e2308bc95fb
Michal Horejsek <horejsekmichal@gmail.com>
2016-02-01 12:31:34 +0100

    Git num aliases

--- msh/git.sh
+++ msh/git.sh
@@ -10,3 +10,11 @@ git config --global push.default current
 
 git config --global alias.l 'log --pretty=format:"%Cgreen%h%Creset %ad %C(cyan)%an%Creset - %s%C(red)%d%Creset" --graph --date=short'
 git config --global alias.ll 'log --pretty=format:"%Cgreen%h%Creset %ad %C(cyan)%an%Creset - %s%C(red)%d%Creset" --graph --date=short --all'
+
+# https://github.com/schreifels/git-num
+alias gn="git num"
+alias ga="git num add"
+alias gr="git num reset"
+alias gco="git num checkout"
+alias gd="git num diff"
+alias gds="git num diff --staged"
