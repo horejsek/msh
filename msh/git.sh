52d6b9f2953288a04e3b198a67e6987d1fa094e7
Michal Hořejšek <horejsekmichal@gmail.com>
2016-05-04 09:08:38 +0200

    New git aliases

--- msh/git.sh
+++ msh/git.sh
@@ -11,6 +11,15 @@ git config --global push.default current
 git config --global alias.l 'log --pretty=format:"%Cgreen%h%Creset %ad %C(cyan)%an%Creset - %s%C(red)%d%Creset" --graph --date=short'
 git config --global alias.ll 'log --pretty=format:"%Cgreen%h%Creset %ad %C(cyan)%an%Creset - %s%C(red)%d%Creset" --graph --date=short --all'
 
+git config --global alias.branches 'branch -a'
+git config --global alias.tags 'tag'
+git config --global alias.stashes 'stash list'
+
+git config --global alias.amend 'commit --amend'
+git config --global alias.uncommit 'reset --soft HEAD~1'
+git config --global alias.unstage 'reset -q HEAD --'
+git config --global alias.discard 'checkout --'
+
 # https://github.com/schreifels/git-num
 alias gn="git num"
 alias ga="git num add"
